7433fa50f603ce7da0535aa8051931a5034aba8e
JuanDulcey <juanedulcey05@gmail.com>
2025-11-03 21:20:33 -0500

    Actualizo application para activar perfil de test

--- src/test/java/com/devv/smartSheduling/SmartShedulingApplicationTests.java
+++ src/test/java/com/devv/smartSheduling/SmartShedulingApplicationTests.java
@@ -2,8 +2,10 @@ package com.devv.smartSheduling;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartShedulingApplicationTests {
 
 	@Test
