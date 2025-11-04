107a9b629a9a89b7cc659d1e66e31b773dbd6797
JuanDulcey <juanedulcey05@gmail.com>
2025-11-03 21:35:56 -0500

    Actualizo spring test para añadir el entorno aislado1

--- src/test/java/com/devv/smartSheduling/SmartShedulingApplicationTests.java
+++ src/test/java/com/devv/smartSheduling/SmartShedulingApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test") //este es un entorno aislado de pruebas QA
+@ActiveProfiles("test") //entorno aisladoQA
 class SmartShedulingApplicationTests {
 
 	@Test
